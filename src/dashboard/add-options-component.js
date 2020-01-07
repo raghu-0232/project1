9a015663c546d00f10cd1a7ba69c038c3fb9e471
Chandra Shekar <chandra.shekar@zaggle.in>
2020-01-07 17:01:07 +0530

    protocol for heroku

--- src/dashboard/add-options-component.js
+++ src/dashboard/add-options-component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import DashboardComponent from './dashboard-component';
 import ShiftsOrdersComponent from '../shifts-orders/shifts-orders.component'
 import List from '@material-ui/core/List';
@@ -7,37 +8,63 @@ import Paper from '@material-ui/core/Paper';
 import './add-options-component.css';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
+
+const DashboardTabs = styled.div`
+    margin: 20px -8px;
+    .MuiPaper-root{
+        padding: 20px;
+    }
+`;
 
 export default function AddOptionsComponents() {
     return( 
-                
-                <List className="add-options-container">
+        <DashboardTabs>
+            <Grid container spacing={2}>
+                <Grid item md={4} lg={4} xs={4} sm={4}>
                     <Paper>
-                    <ListItem> 
-                        {/* <ListItemIcon>
+                        Add Shift Order
+                </Paper>
+                </Grid>
+                <Grid item md={4} lg={4} xs={4} sm={4}>
+                    <Paper>
+                        Add Employees
+                </Paper>
+                </Grid>
+                <Grid item md={4} lg={4} xs={4} sm={4}>
+                    <Paper>
+                        Add Company
+                </Paper>
+                </Grid>
+            </Grid>
+        </DashboardTabs>
+                // <List className="add-options-container">
+                //     <Paper>
+                //     <ListItem> 
+                //         {/* <ListItemIcon>
                            
-                        </ListItemIcon> */}
-                        <ListItemText primary="Add Shift Order" /> 
-                    </ListItem>
-                    </Paper>
-                    <Paper>
-                    <ListItem> 
-                        {/* <ListItemIcon>
+                //         </ListItemIcon> */}
+                //         <ListItemText primary="Add Shift Order" /> 
+                //     </ListItem>
+                //     </Paper>
+                //     <Paper>
+                //     <ListItem> 
+                //         {/* <ListItemIcon>
                           
-                        </ListItemIcon> */}
-                        <ListItemText primary=" Add Employees" /> 
-                    </ListItem>
-                    </Paper>
-                    <Paper>
-                    <ListItem> 
-                        {/* <ListItemIcon>
+                //         </ListItemIcon> */}
+                //         <ListItemText primary=" Add Employees" /> 
+                //     </ListItem>
+                //     </Paper>
+                //     <Paper>
+                //     <ListItem> 
+                //         {/* <ListItemIcon>
                            
-                        </ListItemIcon> */}
-                        <ListItemText primary="Add Company" /> 
-                    </ListItem>
-                    </Paper>
+                //         </ListItemIcon> */}
+                //         <ListItemText primary="Add Company" /> 
+                //     </ListItem>
+                //     </Paper>
                     
-                </List>
+                // </List>
                
         )
  }
